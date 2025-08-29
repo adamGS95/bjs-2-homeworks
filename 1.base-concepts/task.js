60f953d1a0f31d836ced6432371c624895726419
AdamT <a.tupilagov@gmail.com>
2025-08-29 11:02:16 +0300

    commit2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//Задача 1
+
 function solveEquation(a, b, c) {
 	const d = b ** 2 - 4 * a * c;
 
@@ -20,3 +22,34 @@ function testCase() {
 	console.log(solveEquation(1, 1, 1)); // []
 }
 testCase();
+
+//Задача 2
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+	percent = Number(percent);
+	contribution = Number(contribution);
+	amount = Number(amount);
+	countMonths = Number(countMonths);
+
+	
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
+
+	let loanBody = amount - contribution;
+
+	if (loanBody <= 0) {
+		return 0;
+	}
+
+	let monthlyPercent = percent / 100 / 12;
+
+	let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+
+	let totalPayment = monthlyPayment * countMonths + contribution;
+
+	totalPayment = totalPayment.toFixed(2);
+
+	return Number(totalPayment);
+}
