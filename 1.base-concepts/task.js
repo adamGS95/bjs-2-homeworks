255041ea77283d40ea169f00764fc850890b59b0
AdamT <a.tupilagov@gmail.com>
2025-08-29 10:45:08 +0300

    homework quadratic

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return [];
+	} else if (d === 0) {
+		return [-b / (2 * a)];
+	} else {
+		const sqrtD = Math.sqrt(d);
+		return [(-b + sqrtD) / (2 * a), (-b - sqrtD) / (2 * a)];
+	}
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+// Тесты
+function testCase() {
+	console.log(solveEquation(1, -3, 2)); // [2, 1]
+	console.log(solveEquation(1, 2, 1)); // [-1]
+	console.log(solveEquation(1, 1, 1)); // []
 }
+testCase();
