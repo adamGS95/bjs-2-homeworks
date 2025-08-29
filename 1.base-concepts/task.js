607b998ed5e3da8ed92123f523159847cc6828ce
AdamT <a.tupilagov@gmail.com>
2025-08-29 10:54:28 +0300

    homework1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,22 @@
 "use strict";
 
-function solveEquation(a, b, c) {
-	const d = b ** 2 - 4 * a * c;
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
+  countMonths = Number(countMonths);
 
-	if (d < 0) {
-		return [];
-	} else if (d === 0) {
-		return [-b / (2 * a)];
-	} else {
-		const sqrtD = Math.sqrt(d);
-		return [(-b + sqrtD) / (2 * a), (-b - sqrtD) / (2 * a)];
-	}
-}
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
 
-// Тесты
-function testCase() {
-	console.log(solveEquation(1, -3, 2)); // [2, 1]
-	console.log(solveEquation(1, 2, 1)); // [-1]
-	console.log(solveEquation(1, 1, 1)); // []
+  let creditAmount = amount - contribution;
+
+  let monthlyPercent = percent / 100 / 12;
+
+  let monthlyPayment = creditAmount * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+
+  let totalPayment = monthlyPayment * countMonths;
+  
+  return Number(totalPayment.toFixed(2));
 }
-testCase();
