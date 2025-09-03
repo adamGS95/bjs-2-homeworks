283a15450b80cb34fa76074afc5f9f3ad08336f5
AdamT <a.tupilagov@gmail.com>
2025-09-03 21:16:51 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,45 @@
+"use strict";
+
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (let i = 0; i < arr.length; i++) {
+		let num = arr[i];
+
+		if (num > max) {
+			max = num;
+		}
+
+		if (num < min) {
+			min = num;
+		}
+
+		sum = sum + num;
+	}
+
+	let avg = sum / arr.length;
+
+	avg = Number(avg.toFixed(2));
+
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
-function summElementsWorker(...arr) {
+// Функция для проверки работы
+function testCase() {
+	console.log(getArrayParams(-99, 99, 10));
+	// Ожидаем: { min: -99, max: 99, avg: 3.33 }
 
+	console.log(getArrayParams(1, 2, 3, -100, 10));
+	// Ожидаем: { min: -100, max: 10, avg: -16.80 }
+
+	console.log(getArrayParams(5));
+	// Ожидаем: { min: 5, max: 5, avg: 5 }
 }
 
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+testCase();
